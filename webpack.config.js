e3f34d079f8798b53be68cb6e8f8a562243a4bad
caifang <cooldish1245@163.com>
2019-05-06 23:43:30 +0800

    修改uglifyjs相关配置。 处理：npm run build时由于socket.io中es6代码导致的 webpack打包报错

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require('path')
 var webpack = require('webpack')
-
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 module.exports = {
   entry: './src/main.js',
   output: {
@@ -95,10 +95,20 @@ if (process.env.NODE_ENV === 'production') {
         NODE_ENV: '"production"'
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({
+    // new webpack.optimize.UglifyJsPlugin({
+    //   sourceMap: true,
+    //   compress: {
+    //     warnings: false
+    //   }
+    // }),
+
+    new UglifyJsPlugin({
       sourceMap: true,
-      compress: {
-        warnings: false
+      uglifyOptions: {
+        ecma:8,
+        compress: {
+          warnings: false
+        }
       }
     }),
     new webpack.LoaderOptionsPlugin({
